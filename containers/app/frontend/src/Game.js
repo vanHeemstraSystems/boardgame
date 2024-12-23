2e6b2c4f2855d30b468e9db501f71eb2bbb9b524
Willem van Heemstra <wvanheemstra@icloud.com>
2024-12-23 15:28:29 +0000

    Set minMoves and maxMoves to 1

--- containers/app/frontend/src/Game.js
+++ containers/app/frontend/src/Game.js
@@ -3,6 +3,11 @@ import { INVALID_MOVE } from 'boardgame.io/core'
 export const TicTacToe = {
   setup: () => ({ cells: Array(9).fill(null) }),
 
+  turn: {
+    minMoves: 1,
+    maxMoves: 1
+  },
+
   moves: {
     clickCell: ({ G, playerID }, id) => {
       if (G.cells[id] !== null) {
