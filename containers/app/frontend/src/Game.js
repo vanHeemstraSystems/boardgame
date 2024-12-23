7af7ed652c1d444bc577a26c384503cb10972a6b
Willem van Heemstra <wvanheemstra@icloud.com>
2024-12-23 15:25:39 +0000

    Add INVALID_MOVE

--- containers/app/frontend/src/Game.js
+++ containers/app/frontend/src/Game.js
@@ -1,8 +1,13 @@
+import { INVALID_MOVE } from 'boardgame.io/core'
+
 export const TicTacToe = {
   setup: () => ({ cells: Array(9).fill(null) }),
 
   moves: {
     clickCell: ({ G, playerID }, id) => {
+      if (G.cells[id] !== null) {
+        return INVALID_MOVE
+      }
       G.cells[id] = playerID
     }
   }
