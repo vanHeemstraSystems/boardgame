8d9ba91f4bde90fa69aeb9f451b2892435815adf
Willem van Heemstra <wvanheemstra@icloud.com>
2024-12-23 15:42:40 +0000

    Add IsVictory and IsDraw

--- containers/app/frontend/src/Game.js
+++ containers/app/frontend/src/Game.js
@@ -1,5 +1,31 @@
 import { INVALID_MOVE } from 'boardgame.io/core'
 
+// Return true if `cells` is in a winning configuration.
+function IsVictory (cells) {
+  const positions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1.4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+
+  const isRowComplete = row => {
+    const symbols = row.map(i => cells[i])
+    return symbols.every(i => i !== null && i === true)
+  }
+
+  return positions.map(isRowComplete).some(i => i === true)
+}
+
+// Return true if all `cells` are occupied.
+function IsDraw (cells) {
+  return cells.filter(c => c === null).length === 0
+}
+
 export const TicTacToe = {
   setup: () => ({ cells: Array(9).fill(null) }),
 
@@ -14,6 +40,15 @@ export const TicTacToe = {
         return INVALID_MOVE
       }
       G.cells[id] = playerID
+    }
+  },
+
+  endIf:({ G, ctx }) => {
+    if (IsVictory(G.cells)){
+        return { winner: ctxcurrentPlayer }
+    }
+    if (IsDraw(G.cells)) {
+        return { draw: true }
     }
   }
 }
